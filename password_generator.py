1bb41157ea581c7510aec4e8b5f36de01a57f4cf
sai-palvi <syegotvirus01010@gmail.com>
2025-04-22 15:03:58 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,2 +1,9 @@
 import random
 import string
+
+# length of the password
+length = int(input("Enter the length of the password: "))
+
+chars = input("Do you want to add characters? (yes or no): ")
+case = input("Uppercase or Lowercase? [U/L]: ")
+specials = input("Do you want special characters? (yes or no): ")
