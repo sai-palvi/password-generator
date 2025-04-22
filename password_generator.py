f932d4413169260b5deb2318123d277dd5f58e89
sai-palvi <syegotvirus01010@gmail.com>
2025-04-22 15:21:57 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,22 @@
 import random
 import string
+digits = "0123456789"
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+special_chars = "!@#$%^&*"
+#input
+include_digits = input("Do you want digits in your password? (yes/no): ").lower() == "yes"
+include_letters = input("Do you want letters in your password? (yes/no): ").lower() == "yes"
+include_special = input("Do you want special characters in your password? (yes/no): ").lower() == "yes"
 
-# length of the password
-length = int(input("Enter the length of the password: "))
 
-chars = input("Do you want to add characters? (yes or no): ")
-case = input("Uppercase or Lowercase? [U/L]: ")
-specials = input("Do you want special characters? (yes or no): ")
+pool = ""
+if include_digits:
+    pool += digits
+if include_letters:
+    pool += letters
+if include_special:
+    pool += special_chars
+length = int(input("Enter the password lenght: "))
+password = ''.join(random.choice(pool) for _ in range(length))
+print("Your password is: "+password)  
+  
