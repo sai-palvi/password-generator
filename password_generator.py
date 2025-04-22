007104db2954eb1c97a210d52fcabfe0ead295b3
sai-palvi <syegotvirus01010@gmail.com>
2025-04-22 15:37:26 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,28 @@
 import random
 import string
+print(r"""
+  _____                                    _    
+ |  __ \                                  | |   
+ | |__) |_ _ ___ _____      _____  _ __ __| |   
+ |  ___/ _` / __/ __\ \ /\ / / _ \| '__/ _` |   
+ | |  | (_| \__ \__ \\ V  V / (_) | | | (_| |   
+ |_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_|   
+                                 | |            
+   __ _  ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
+  / _` |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+ | (_| |  __/ | | |  __/ | | (_| | || (_) | |   
+  \__, |\___|_| |_|\___|_|  \__,_|\__\___/|_|   
+   __/ |                                        
+  |___/               
+  
+  Password generator by Sai and Arnik
+                            
+""")
+
 digits = "0123456789"
 letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 special_chars = "!@#$%^&*"
 #input
-include_digits = input ("Welcome to password generator !")
 include_digits = input("Do you want digits in your password? (yes/no): ").lower() == "yes"
 include_letters = input("Do you want letters in your password? (yes/no): ").lower() == "yes"
 include_special = input("Do you want special characters in your password? (yes/no): ").lower() == "yes"
@@ -19,7 +37,5 @@ if include_special:
     pool += special_chars
 length = int(input("Enter the password lenght: "))
 password = ''.join(random.choice(pool) for _ in range(length))
-print("Your password is: "+password)  
-       
-include_digits = input(" Thankyou for using our code ") 
-include_digits = input(" Visit Again ! ") 
+print("Your password is: "+password)
+
