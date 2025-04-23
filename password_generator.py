f4d58ff1817ae964daa087a401da74fd89e3afde
jainarnik451 <jainarnik451@gmail.com>
2025-04-23 22:41:32 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,6 @@
 import random
 import string
+
 print(r"""
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 * _____                                    _            *
@@ -15,30 +16,39 @@ print(r"""
 * \__, |\___|_| |_|\___|_|  \__,_|\__\___/|_|           *
 *  __/ |                                                *
 * |___/                                                 *
-* Password generator by Sai and Arnik                   *
+* Password generator by Sai &&  Arnik                   *
 *                                                       *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
-  
-                            
 """)
 
-digits = "0123456789"
-letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-special_chars = "!@#$%^&*"
-#input
-include_digits = input("Do you want digits in your password? (yes/no): ").lower() == "yes"
-include_letters = input("Do you want letters in your password? (yes/no): ").lower() == "yes"
-include_special = input("Do you want special characters in your password? (yes/no): ").lower() == "yes"
+def main():
+    digits = "0123456789"
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    special_chars = "!@#$%^&*,<>./?';}+=-_"
+    
+    # Input
+    include_digits = input("Do you want digits in your password? (yes/no): ").lower() == "yes"
+    include_letters = input("Do you want letters in your password? (yes/no): ").lower() == "yes"
+    include_special = input("Do you want special characters in your password? (yes/no): ").lower() == "yes"
 
+    pool = ""
+    if include_digits:
+        pool += digits
+    if include_letters:
+        pool += letters
+    if include_special:
+        pool += special_chars
 
-pool = ""
-if include_digits:
-    pool += digits
-if include_letters:
-    pool += letters
-if include_special:
-    pool += special_chars
-length = int(input("Enter the password lenght: "))
-password = ''.join(random.choice(pool) for _ in range(length))
-print("Your password is: "+password)
+    length = int(input("Enter the password length: "))
+    
+    if length <= 0 or len(pool) == 0:
+        print("Invalid password length or no character types selected.")
+        return
 
+    password = ''.join(random.choice(pool) for _ in range(length))
+    print("Your password is: " + password)
+
+if __name__ == "__main__":
+    main()
+    print("Thank you!")
+    print("Visit Again!")
